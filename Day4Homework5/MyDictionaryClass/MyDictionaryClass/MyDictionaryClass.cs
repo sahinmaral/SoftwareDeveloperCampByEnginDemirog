9cf9ce8b536dd4506184f1b39bf471456bd5209f
sahinmaral <sahin.maral@hotmail.com>
2021-04-11 20:40:20 +0300

    Added Homework 5 on Day 4

---
+++ Day4Homework5/MyDictionaryClass/MyDictionaryClass/MyDictionaryClass.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryClass
+{
+    class MyDictionaryClass<T1, T2>
+    {
+        T1[] key;
+        T2[] value;
+
+        public MyDictionaryClass()
+        {
+            key = new T1[0];
+            value = new T2[0];
+        }
+        public void Add(T1 keyitem, T2 valueitem)
+        {
+            bool isKeyExist = ContainsKey(keyitem);
+            if (!isKeyExist)
+            {
+                T1[] tempKey = key;
+                T2[] tempValue = value;
+                key = new T1[key.Length + 1];
+                value = new T2[value.Length + 1];
+
+                for (int i = 0; i < tempKey.Length; i++)
+                {
+                    key[i] = tempKey[i];
+                }
+                for (int i = 0; i < tempValue.Length; i++)
+                {
+                    value[i] = tempValue[i];
+                }
+
+                key[key.Length - 1] = keyitem;
+                value[value.Length - 1] = valueitem;
+            }
+
+            else
+            {
+                throw new Exception("Same key already has been declared");
+            }
+
+        }
+        public bool ContainsKey(T1 keyitem)
+        {
+            bool isThere = false;
+            foreach (var item in key)
+            {
+                if (item.ToString() == keyitem.ToString())
+                {
+                    isThere = true;
+                }
+                else
+                {
+                    isThere = false;
+                }
+            }
+            return isThere;
+        }
+        public bool ContainsValue(T2 valueitem)
+        {
+            bool isThere = false;
+            foreach (var item in value)
+            {
+                if (item.ToString() == valueitem.ToString())
+                {
+                    isThere = true;
+                }
+                else
+                {
+                    isThere = false;
+                }
+            }
+            return isThere;
+        }
+        public int KeyLength { get { return key.Length; } }
+        public int ValueLength { get { return value.Length; } }
+        public T1[] Keys { get { return key; } }
+        public T2[] Values { get { return value; } }
+    }
+}
---
+++ Day4Homework5/MyDictionaryClass/MyDictionaryClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionaryClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryClass<int, string> isimler = new MyDictionaryClass<int, string>();
+            isimler.Add(150, "Şahin");
+            isimler.Add(150, "Şahin");  //We will get error because same key has been already declared
+            isimler.Add(151, "Ali");
+            isimler.Add(152, "Batuhan");
+            isimler.Add(153, "Mehmet");
+
+            for (int i = 0; i < isimler.KeyLength; i++)
+            {
+                Console.WriteLine("{0} , {1}", isimler.Keys[i],isimler.Values[i]);
+            }
+
+
+        }
+    }
+}
