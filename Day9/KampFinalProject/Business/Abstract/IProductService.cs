2b47cc87a590bc9e28fa0eb094a6339bab9a11d4
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-05-12 16:42:37 +0300

    Added Day 9 Summaries

---
+++ Day9/KampFinalProject/Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.DTOs;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int id);
+        List<Product> GetByUnitPrice(decimal min, decimal max);
+        List<ProductDetailDto> GetProductDetails();
+
+    }
+}
---
+++ Day9/KampFinalProject/Business/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Business.Abstract;
+
+using DataAccess.Abstract;
+
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.Get(x => x.CategoryId == categoryId);
+        }
+    }
+}
