106b565a781fc23aaba76a947d0744c3b5d4d8b9
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-05-14 15:39:17 +0300

    Added Day 11 Summaries

---
+++ Day11/KampFinalProject/DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Core.DataAccess;
+
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        List<ProductDetailDto> GetProductDetails();
+    }
+}
---
+++ Day11/KampFinalProject/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Core.DataAccess.EntityFramework;
+using Entities.DTOs;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //NuGet
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                    join c in context.Categories on p.CategoryId equals c.CategoryId
+                    select new ProductDetailDto
+                    {
+                        ProductId = p.ProductId,
+                        ProductName = p.ProductName,
+                        CategoryName = c.CategoryName,
+                        UnitsInStock = p.UnitsInStock
+                    };
+                return result.ToList();
+            }
+
+            
+        }
+    }
+}
