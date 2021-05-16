7eaae1d021f4813131bfef425b1d97558ead9dae
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-05-17 00:14:58 +0300

    Added Day 12 Homework 2

---
+++ Day12Homework2/CarRental/Business/ValidationRules/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(x => x.CarName).NotEmpty();
+            RuleFor(x => x.BrandId).NotEmpty();
+            RuleFor(x => x.ColourId).NotEmpty();
+            RuleFor(x => x.DailyPrice).GreaterThan(0);
+            RuleFor(x => x.CarName).MinimumLength(2);
+        }
+    }
+}
