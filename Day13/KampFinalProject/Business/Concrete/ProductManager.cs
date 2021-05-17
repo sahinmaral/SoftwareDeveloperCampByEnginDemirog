62c8eeb1c7d505048e637bce366edb0b2607a702
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-05-17 17:55:03 +0300

    Added Day 13 Summaries

---
+++ Day13/KampFinalProject/Business/Concrete/ProductManager.cs
@@ -0,0 +1,126 @@
+﻿using Business.Abstract;
+using Business.Constants;
+
+using Core.Utilities.Results;
+
+using DataAccess.Abstract;
+
+using Entities.Concrete;
+using Entities.DTOs;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.Business;
+
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        private ICategoryService _categoryService;
+
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
+        {
+            _productDal = productDal;
+            _categoryService = categoryService;
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            if (DateTime.Now.Hour == 22)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
+
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductListed);
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Add(Product product)
+        {
+            var result = BusinessRules.Run(
+                CheckIfProductCountOfCategoryOverload(product.CategoryId),
+                CheckIfExistSameProductName(product.ProductName), CheckCategoryOverload());
+
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+
+        }
+
+        private IResult CheckCategoryOverload()
+        {
+            var result = _categoryService.GetAll().Data.Count();
+            if (result >= 15)
+            {
+                return new ErrorResult(Messages.CategoryOverload);
+            }
+
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfExistSameProductName(string productName)
+        {
+            var result = _productDal.GetAll(x => x.ProductName == productName).Any();
+            if (!result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfProductCountOfCategoryOverload(int categoryId)
+        {
+            int result = _productDal.GetAll().Count(x => x.CategoryId == categoryId);
+            if (result + 1 >= 30)
+            {
+                return new ErrorResult(Messages.ProductCategoryOverload);
+            }
+            return new SuccessResult();
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(x => x.ProductId == productId));
+        }
+
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
+        }
+
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
+        {
+            return new SuccessDataResult<List<Product>>(
+                _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
+        }
+
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
+        {
+            if (DateTime.Now.Hour == 17)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
+
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+    }
+}
---
+++ Day13/KampFinalProject/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using Business.Abstract;
+using Business.CCS;
+using Business.Concrete;
+
+using DataAccess.Concrete.EntityFramework;
+
+using Entities.Concrete;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            IProductService productService = new ProductManager(new EfProductDal(),
+                new CategoryManager(new EfCategoryDal()));
+
+            var result = productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
