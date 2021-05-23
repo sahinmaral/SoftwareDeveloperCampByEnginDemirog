537ba10f2c4e4320b6fe74446b6abad3c6786e40
Sahin MARAL <74562862+sahinmaral@users.noreply.github.com>
2021-05-23 12:35:26 +0300

    Added Day 15 Summaries

---
+++ Day15/KampFinalProject/Entities/Concrete/Category.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    //Çıplak Class Kalmasın
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
